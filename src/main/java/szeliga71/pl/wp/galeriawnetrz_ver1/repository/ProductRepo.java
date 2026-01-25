ed2433ff59d73b42e1c72eab929f0e58e9611bfa
Stanislaw Malewicz <szeliga71@wp.pl>
2026-01-25 22:41:51 +0100

    repo poprawione

--- src/main/java/szeliga71/pl/wp/galeriawnetrz_ver1/repository/ProductRepo.java
+++ src/main/java/szeliga71/pl/wp/galeriawnetrz_ver1/repository/ProductRepo.java
@@ -1,5 +1,7 @@
 package szeliga71.pl.wp.galeriawnetrz_ver1.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,13 +20,13 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
 
     // Szukanie po nazwie kategorii
     List<Product> findByCategoryNameIgnoreCase(String categoryName);
-
+    Page<Product> findByCategoryNameIgnoreCase(String categoryName, Pageable pageable);
     // Szukanie po nazwie podkategorii
     List<Product> findBySubCategoryNameIgnoreCase(String subCategoryName);
 
     // Szukanie po nazwie brandu
     List<Product> findByBrandNameIgnoreCase(String brandName);
-
+    Page<Product> findByBrandNameIgnoreCase(String brandName, Pageable pageable);
     // Szukanie po kategorii i podkategorii razem
     List<Product> findByCategoryNameIgnoreCaseAndSubCategoryNameIgnoreCase(String categoryName, String subCategoryName);
 
@@ -39,3 +41,42 @@ public interface ProductRepo extends JpaRepository<Product, Long> {
     //List<Product> findByNameIgnoreCase(String name);
 
 }
+/*
+package szeliga71.pl.wp.galeriawnetrz_ver1.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import szeliga71.pl.wp.galeriawnetrz_ver1.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    // Paginowane wyszukiwanie
+    Page<Product> findByCategoryNameIgnoreCase(String categoryName, Pageable pageable);
+
+    Page<Product> findBySubCategoryNameIgnoreCase(String subCategoryName, Pageable pageable);
+
+    Page<Product> findByBrandNameIgnoreCase(String brandName, Pageable pageable);
+
+    Page<Product> findByCategoryNameIgnoreCaseAndSubCategoryNameIgnoreCase(String categoryName, String subCategoryName, Pageable pageable);
+
+    List<Product> findByBrandNameIgnoreCase(String brandName);
+    List<Product> findBySubCategoryNameIgnoreCase(String subCategoryName);
+    List<Product> findByCategoryNameIgnoreCase(String categoryName);
+    List<Product> findByCategoryNameIgnoreCaseAndSubCategoryNameIgnoreCase(String categoryName, String subCategoryName);
+
+    // Bez paginacji
+    List<Product> findByNameIgnoreCaseContaining(String query);
+
+
+
+
+*/
