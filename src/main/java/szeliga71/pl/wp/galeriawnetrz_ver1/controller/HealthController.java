895d50bcb586fdff88b0eea933e394ace2d8d99c
Stanislaw Malewicz <szeliga71@wp.pl>
2026-01-25 22:19:02 +0100

    health controller

---
+++ src/main/java/szeliga71/pl/wp/galeriawnetrz_ver1/controller/HealthController.java
@@ -0,0 +1,17 @@
+package szeliga71.pl.wp.galeriawnetrz_ver1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
+
